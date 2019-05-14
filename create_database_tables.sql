5f8aa04983cb9b309afdc21d081ed0fc12eefb42
vfigu <vicente.figueroa.950@my.csun.edu>
2019-05-14 07:35:56 +0000

    Connects table schema with appropriate foreign keys. Updates create, drop, and seed procedures.

--- create_database_tables.sql
+++ create_database_tables.sql
@@ -7,33 +7,41 @@ CREATE PROCEDURE create_database_tables
 AS
 BEGIN
 	/****** Script for creating Maintenance Table  ******/
+	DROP TABLE IF EXISTS [dbo].[Maintenance]
 	CREATE TABLE [dbo].[Maintenance] (
-		[Work Item Type] nvarchar(255),
+		[Work Item Type ID] int,
 		[Maintenance ID] int,
 		[Title] nvarchar(255),
 		[Assigned To] int,
-		[State] nvarchar(255),
+		[State ID] int,
 		[Area Path] nvarchar(255),
 		[Comment Count] int,
 		[Created Date] datetime,
 		[Accepted By] int,
-		[Accepted Date] nvarchar(255),
-		[Activity] nvarchar(255),
+		[Accepted Date] datetime,
+		[Activity ID] int,
 		[Priority] int,
 		[Description] nvarchar(max),
 		[System ID] int,
 		[ChildOf] int,
+
+		PRIMARY KEY ([Maintenance ID]),
 	)
 
 	/****** Script for creating Release Table  ******/
+	DROP TABLE IF EXISTS [dbo].[Release]
 	CREATE TABLE [dbo].[Release] (
-		[Release Num] float,
+		[Release ID] int,
+		[Release Num] int,
 		[Release Name] nvarchar(255),
 		[Release Date] datetime,
 		[Associated Features] int,
+
+		PRIMARY KEY ([Release ID]),
 	)
 
 	/****** Script for creating System Table  ******/
+	DROP TABLE IF EXISTS [dbo].[System]
 	CREATE TABLE [dbo].[System] (
 		[System ID] int,
 		[Abbreviation] nvarchar(255),
@@ -43,25 +51,28 @@ BEGIN
 	)
 
 	/****** Script for creating Product Table  ******/
+	DROP TABLE IF EXISTS [dbo].[Product]
 	CREATE TABLE [dbo].[Product] (
 		[Product ID] int,
 		[Product Name] nvarchar(255),
 		[Product Description] nvarchar(max),
-		[Product Type ID] nvarchar(255),
-		[Product Schema ID] nvarchar(255),
+		[Product Type ID] int,
+		[Product Schema ID] int,
 		[Product Log] nvarchar(255),
 		[Approved] nvarchar(10),
 
-		PRIMARY KEY ([Product ID])
+		PRIMARY KEY ([Product ID]),
 	)
 	
 	/****** Script for creating Version Table  ******/
+	DROP TABLE IF EXISTS [dbo].[Version]
 	CREATE TABLE [dbo].[Version] (
 		[Product ID] int,
-		[Release Num] int,
+		[Release ID] int,
 	)
 	
 	/****** Script for creating Feature Table  ******/
+	DROP TABLE IF EXISTS [dbo].[Feature]
 	CREATE TABLE [dbo].[Feature] (
 		[Feature ID] int,
 		[Feature Name] nvarchar(255),
@@ -73,6 +84,7 @@ BEGIN
 	)
 	
 	/****** Script for creating Work Item Type Table  ******/
+	DROP TABLE IF EXISTS [dbo].[Work Item Type]
 	CREATE TABLE [dbo].[Work Item Type] (
 		[Work Item Type ID] int,
 		[Work Item Type Name] nvarchar(255),
@@ -82,6 +94,7 @@ BEGIN
 	)
 	
 	/****** Script for creating Stakeholder Table  ******/
+	DROP TABLE IF EXISTS [dbo].[Stakeholder]
 	CREATE TABLE [dbo].[Stakeholder] (
 		[Stakeholder ID] int,
 		[Stakeholder Name] nvarchar(255),
@@ -91,6 +104,7 @@ BEGIN
 	)
 
 	/****** Script for creating State Table  ******/
+	DROP TABLE IF EXISTS [dbo].[State]
 	CREATE TABLE [dbo].[State] (
 		[State ID] int,
 		[State Name] nvarchar(255),
@@ -100,6 +114,7 @@ BEGIN
 	)
 	
 	/****** Script for creating Activity Table  ******/
+	DROP TABLE IF EXISTS [dbo].[Activity]
 	CREATE TABLE [dbo].[Activity] (
 		[Activity ID] int,
 		[Activity Name] nvarchar(255),
@@ -107,9 +122,9 @@ BEGIN
 
 		PRIMARY KEY ([Activity ID])
 	)
-
 	
 	/****** Script for creating Product Type Table  ******/
+	DROP TABLE IF EXISTS [dbo].[Product Type]
 	CREATE TABLE [dbo].[Product Type] (
 		[Product Type ID] int,
 		[Product Type Name] nvarchar(255),
@@ -117,8 +132,9 @@ BEGIN
 
 		PRIMARY KEY ([Product Type ID])
 	)
-	
+		
 	/****** Script for creating Product Schema Table  ******/
+	DROP TABLE IF EXISTS [dbo].[Product Schema]
 	CREATE TABLE [dbo].[Product Schema] (
 		[Product Schema ID] int,
 		[Product Schema Name] nvarchar(255),
@@ -128,6 +144,7 @@ BEGIN
 	)
 			
 	/****** Script for creating Feature Type Table  ******/
+	DROP TABLE IF EXISTS [dbo].[Feature Type]
 	CREATE TABLE [dbo].[Feature Type] (
 		[Feature Type ID] int,
 		[Feature Type Name] nvarchar(255),
@@ -135,5 +152,51 @@ BEGIN
 
 		PRIMARY KEY ([Feature Type ID])
 	)
+	
+	/****** Script for setting FK on Maintenance Table  ******/
+	ALTER TABLE [dbo].[Maintenance]
+		ADD CONSTRAINT FK__WorkItemTypeID
+		FOREIGN KEY ([Work Item Type ID]) REFERENCES [Work Item Type]([Work Item Type ID])
+	ALTER TABLE [dbo].[Maintenance]
+		ADD CONSTRAINT FK__AssignedTo
+		FOREIGN KEY ([Assigned To]) REFERENCES [Stakeholder]([Stakeholder ID])
+	ALTER TABLE [dbo].[Maintenance]
+		ADD CONSTRAINT FK__State
+		FOREIGN KEY ([State ID]) REFERENCES [State]([State ID])
+	ALTER TABLE [dbo].[Maintenance]
+		ADD CONSTRAINT FK__AcceptedBy
+		FOREIGN KEY ([Accepted By]) REFERENCES [Stakeholder]([Stakeholder ID])
+	ALTER TABLE [dbo].[Maintenance]
+		ADD CONSTRAINT FK__Activity
+		FOREIGN KEY ([Activity ID]) REFERENCES [Activity]([Activity ID])
+	ALTER TABLE [dbo].[Maintenance]
+		ADD CONSTRAINT FK__SystemID
+		FOREIGN KEY ([System ID]) REFERENCES [System]([System ID])
+	
+	/****** Script for setting FK on Release Table  ******/
+	ALTER TABLE [dbo].[Release]
+		ADD CONSTRAINT FK__AssociatedFeatures
+		FOREIGN KEY ([Associated Features]) REFERENCES [dbo].[Feature]([Feature ID])
+	
+	/****** Script for setting FK on Product Table  ******/
+	ALTER TABLE [dbo].[Product]
+		ADD CONSTRAINT FK__ProductTypeID
+		FOREIGN KEY ([Product Type ID]) REFERENCES [dbo].[Product Type]([Product Type ID])
+	ALTER TABLE [dbo].[Product]
+		ADD CONSTRAINT FK__ProductSchemaID
+		FOREIGN KEY ([Product Schema ID]) REFERENCES [dbo].[Product Schema]([Product Schema ID])
+	
+	/****** Script for setting FK on Version Table  ******/
+	ALTER TABLE [dbo].[Version]
+		ADD CONSTRAINT FK__ProductID
+		FOREIGN KEY ([Product ID]) REFERENCES [dbo].[Product]([Product ID])
+	ALTER TABLE [dbo].[Version]
+		ADD CONSTRAINT FK__ReleaseID
+		FOREIGN KEY ([Release ID]) REFERENCES [dbo].[Release]([Release ID])
+	
+	/****** Script for setting FK on Feature Table  ******/
+	ALTER TABLE [dbo].[Feature]
+		ADD CONSTRAINT FK__FeatureType
+		FOREIGN KEY ([Feature Type ID]) REFERENCES [dbo].[Feature Type]([Feature Type ID])
 END
 GO
--- drop_database_tables.sql
+++ drop_database_tables.sql
@@ -6,43 +6,78 @@
 CREATE PROCEDURE drop_database_tables
 AS
 BEGIN
+
+	/****** Script for setting FK on Maintenance Table  ******/
+	ALTER TABLE [dbo].[Maintenance]
+		DROP CONSTRAINT IF EXISTS FK__WorkItemType
+	ALTER TABLE [dbo].[Maintenance]
+		DROP CONSTRAINT IF EXISTS FK__AssignedTo
+	ALTER TABLE [dbo].[Maintenance]
+		DROP CONSTRAINT IF EXISTS FK__State
+	ALTER TABLE [dbo].[Maintenance]
+		DROP CONSTRAINT IF EXISTS FK__AcceptedBy
+	ALTER TABLE [dbo].[Maintenance]
+		DROP CONSTRAINT IF EXISTS FK__Activity
+	ALTER TABLE [dbo].[Maintenance]
+		DROP CONSTRAINT IF EXISTS FK__SystemID
+	
+	/****** Script for setting FK on Release Table  ******/
+	ALTER TABLE [dbo].[Release]
+		DROP CONSTRAINT IF EXISTS FK__AssociatedFeatures
+	
+	/****** Script for setting FK on Product Table  ******/
+	ALTER TABLE [dbo].[Product]
+		DROP CONSTRAINT IF EXISTS FK__ProductTypeID
+	ALTER TABLE [dbo].[Product]
+		DROP CONSTRAINT IF EXISTS FK__ProductSchemaID
+	
+	/****** Script for setting FK on Version Table  ******/
+	ALTER TABLE [dbo].[Version]
+		DROP CONSTRAINT IF EXISTS FK__ProductID
+	ALTER TABLE [dbo].[Version]
+		DROP CONSTRAINT IF EXISTS FK__ReleaseNum
+	
+	/****** Script for setting FK on Feature Table  ******/
+	ALTER TABLE [dbo].[Feature]
+		DROP CONSTRAINT IF EXISTS FK__FeatureType
+
 	/****** Script for dropping Maintenance Table  ******/
-	DROP TABLE [dbo].[Maintenance];
+	DROP TABLE IF EXISTS [dbo].[Maintenance];
 
 	/****** Script for dropping Release Table  ******/
-	DROP TABLE [dbo].[Release] ;
+	DROP TABLE IF EXISTS [dbo].[Release] ;
 
 	/****** Script for dropping System Table  ******/
-	DROP TABLE [dbo].[System];
+	DROP TABLE IF EXISTS [dbo].[System];
 
 	/****** Script for dropping Product Table  ******/
-	DROP TABLE [dbo].[Product];
+	DROP TABLE IF EXISTS [dbo].[Product];
 	
 	/****** Script for dropping Version Table  ******/
-	DROP TABLE [dbo].[Version];
+	DROP TABLE IF EXISTS [dbo].[Version];
 	
 	/****** Script for dropping Feature Table  ******/
-	DROP TABLE [dbo].[Feature];
+	DROP TABLE IF EXISTS [dbo].[Feature];
 	
 	/****** Script for dropping Work Item Type Table  ******/
-	DROP TABLE [dbo].[Work Item Type];
+	DROP TABLE IF EXISTS [dbo].[Work Item Type];
 	
 	/****** Script for dropping Stakeholder Table  ******/
-	DROP TABLE [dbo].[Stakeholder];
+	DROP TABLE IF EXISTS [dbo].[Stakeholder];
 
 	/****** Script for dropping State Table  ******/
-	DROP TABLE [dbo].[State];
+	DROP TABLE IF EXISTS [dbo].[State];
 	
 	/****** Script for dropping Activity Table  ******/
-	DROP TABLE [dbo].[Activity];
+	DROP TABLE IF EXISTS [dbo].[Activity];
 
 	/****** Script for dropping Product Type Table  ******/
-	DROP TABLE [dbo].[Product Type];
+	DROP TABLE IF EXISTS [dbo].[Product Type];
 	
 	/****** Script for dropping Product Schema Table  ******/
-	DROP TABLE [dbo].[Product Schema];
+	DROP TABLE IF EXISTS [dbo].[Product Schema];
 
 	/****** Script for dropping Feature Type Table  ******/
-	DROP TABLE [dbo].[Feature Type];
+	DROP TABLE IF EXISTS [dbo].[Feature Type];
 END
 GO
